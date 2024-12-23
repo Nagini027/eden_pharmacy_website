a37ea20b39057dfd56c24b0f1409c161dbd9d7f5
Nagini027 <nagalakshmis027@gmail.com>
2024-12-24 03:56:42 +0530

    navigation bar updated

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,7 +5,7 @@ const Navbar = () => {
   return (
     <header className="md:pl-7">
       <div className="md:pl-36">
-        <Image src="/images/logo.png" alt="Logo" width={250} height={64} />
+        <Image src="/images/image (1).png" alt="Logo" width={200} height={64} />
       </div>
     </header>
   );
